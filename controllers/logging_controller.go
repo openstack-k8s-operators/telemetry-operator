c9535247ce9ff1268718776a7890b0500e778d4e
jlarriba <jlarriba@redhat.com>
2023-11-21 11:52:14 +0100

    Added Logging configuration to telemetry-operator

---
+++ controllers/logging_controller.go
@@ -0,0 +1,332 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	logr "github.com/go-logr/logr"
+	common "github.com/openstack-k8s-operators/lib-common/modules/common"
+	condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
+	env "github.com/openstack-k8s-operators/lib-common/modules/common/env"
+	helper "github.com/openstack-k8s-operators/lib-common/modules/common/helper"
+	labels "github.com/openstack-k8s-operators/lib-common/modules/common/labels"
+	common_rbac "github.com/openstack-k8s-operators/lib-common/modules/common/rbac"
+	secret "github.com/openstack-k8s-operators/lib-common/modules/common/secret"
+	util "github.com/openstack-k8s-operators/lib-common/modules/common/util"
+
+	telemetryv1 "github.com/openstack-k8s-operators/telemetry-operator/api/v1beta1"
+	ceilometer "github.com/openstack-k8s-operators/telemetry-operator/pkg/ceilometer"
+	logging "github.com/openstack-k8s-operators/telemetry-operator/pkg/logging"
+)
+
+// LoggingReconciler reconciles a Logging object
+type LoggingReconciler struct {
+	client.Client
+	Kclient kubernetes.Interface
+	Scheme  *runtime.Scheme
+}
+
+// GetLogger returns a logger object with a prefix of "conroller.name" and aditional controller context fields
+func (r *LoggingReconciler) GetLogger(ctx context.Context) logr.Logger {
+	return log.FromContext(ctx).WithName("Controllers").WithName("Logging")
+}
+
+//+kubebuilder:rbac:groups=telemetry.openstack.org,resources=loggings,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=telemetry.openstack.org,resources=loggings/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=telemetry.openstack.org,resources=loggings/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete;
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete;
+//+kubebuilder:rbac:groups=dataplane.openstack.org,resources=openstackdataplanenodesets,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Logging object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
+func (r *LoggingReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, _err error) {
+	Log := r.GetLogger(ctx)
+
+	// Fetch the Logging instance
+	instance := &telemetryv1.Logging{}
+	err := r.Client.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		if k8s_errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers. Return and don't requeue.
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+
+	helper, err := helper.NewHelper(
+		instance,
+		r.Client,
+		r.Kclient,
+		r.Scheme,
+		Log,
+	)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Always patch the instance status when exiting this function so we can persist any changes.
+	defer func() {
+		// update the Ready condition based on the sub conditions
+		if instance.Status.Conditions.AllSubConditionIsTrue() {
+			instance.Status.Conditions.MarkTrue(
+				condition.ReadyCondition, condition.ReadyMessage)
+		} else {
+			// something is not ready so reset the Ready condition
+			instance.Status.Conditions.MarkUnknown(
+				condition.ReadyCondition, condition.InitReason, condition.ReadyInitMessage)
+			// and recalculate it based on the state of the rest of the conditions
+			instance.Status.Conditions.Set(
+				instance.Status.Conditions.Mirror(condition.ReadyCondition))
+		}
+		err := helper.PatchInstance(ctx, instance)
+		if err != nil {
+			_err = err
+			return
+		}
+	}()
+
+	// If we're not deleting this and the service object doesn't have our finalizer, add it.
+	if instance.DeletionTimestamp.IsZero() && controllerutil.AddFinalizer(instance, helper.GetFinalizer()) {
+		return ctrl.Result{}, nil
+	}
+
+	//
+	// initialize status
+	//
+	if instance.Status.Conditions == nil {
+		instance.Status.Conditions = condition.Conditions{}
+		// initialize conditions used later as Status=Unknown
+		cl := condition.CreateList(
+			condition.UnknownCondition(condition.InputReadyCondition, condition.InitReason, condition.InputReadyInitMessage),
+			condition.UnknownCondition(condition.ServiceConfigReadyCondition, condition.InitReason, condition.ServiceConfigReadyInitMessage),
+			condition.UnknownCondition(condition.DeploymentReadyCondition, condition.InitReason, condition.DeploymentReadyInitMessage),
+			// service account, role, rolebinding conditions
+			condition.UnknownCondition(condition.ServiceAccountReadyCondition, condition.InitReason, condition.ServiceAccountReadyInitMessage),
+			condition.UnknownCondition(condition.RoleReadyCondition, condition.InitReason, condition.RoleReadyInitMessage),
+			condition.UnknownCondition(condition.RoleBindingReadyCondition, condition.InitReason, condition.RoleBindingReadyInitMessage),
+			// right now we have no dedicated KeystoneServiceReadyInitMessage
+			condition.UnknownCondition(condition.KeystoneServiceReadyCondition, condition.InitReason, ""),
+		)
+
+		instance.Status.Conditions.Init(&cl)
+
+		// Register overall status immediately to have an early feedback e.g. in the cli
+		return ctrl.Result{}, nil
+	}
+
+	if instance.Status.Hash == nil {
+		instance.Status.Hash = map[string]string{}
+	}
+
+	// Handle service delete
+	if !instance.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(ctx, instance, helper)
+	}
+
+	// Handle non-deleted clusters
+	return r.reconcileNormal(ctx, instance, helper)
+}
+
+func (r *LoggingReconciler) reconcileDelete(ctx context.Context, instance *telemetryv1.Logging, helper *helper.Helper) (ctrl.Result, error) {
+	Log := r.GetLogger(ctx)
+	Log.Info("Reconciling Service delete")
+
+	// Service is deleted so remove the finalizer.
+	controllerutil.RemoveFinalizer(instance, helper.GetFinalizer())
+	Log.Info(fmt.Sprintf("Reconciled Service '%s' delete successfully", logging.ServiceName))
+
+	return ctrl.Result{}, nil
+}
+
+func (r *LoggingReconciler) reconcileNormal(ctx context.Context, instance *telemetryv1.Logging, helper *helper.Helper) (ctrl.Result, error) {
+	Log := r.GetLogger(ctx)
+	Log.Info(fmt.Sprintf("Reconciling Service '%s'", logging.ServiceName))
+
+	// Service account, role, binding
+	rbacRules := []rbacv1.PolicyRule{
+		{
+			APIGroups:     []string{"security.openshift.io"},
+			ResourceNames: []string{"anyuid"},
+			Resources:     []string{"securitycontextconstraints"},
+			Verbs:         []string{"use"},
+		},
+		{
+			APIGroups: []string{""},
+			Resources: []string{"pods"},
+			Verbs:     []string{"create", "get", "list", "watch", "update", "patch", "delete"},
+		},
+	}
+	rbacResult, err := common_rbac.ReconcileRbac(ctx, helper, instance, rbacRules)
+	if err != nil {
+		return rbacResult, err
+	} else if (rbacResult != ctrl.Result{}) {
+		return rbacResult, nil
+	}
+
+	instance.Status.Conditions.MarkTrue(condition.InputReadyCondition, condition.InputReadyMessage)
+
+	// ConfigMap
+	configMapVars := make(map[string]env.Setter)
+
+	//
+	// create Configmap required for logging-compute input
+	// - %-config configmap holding minimal logging config required to get the service up, user can add additional files to be added to the service
+	//
+	err = r.generateComputeServiceConfig(ctx, helper, instance, &configMapVars)
+	if err != nil {
+		instance.Status.Conditions.Set(condition.FalseCondition(
+			condition.ServiceConfigReadyCondition,
+			condition.ErrorReason,
+			condition.SeverityWarning,
+			condition.ServiceConfigReadyErrorMessage,
+			err.Error()))
+		return ctrl.Result{}, err
+	}
+
+	//
+	// create hash over all the different input resources to identify if any those changed
+	// and a restart/recreate is required.
+	//
+	inputHash, hashChanged, err := r.createHashOfInputHashes(ctx, instance, configMapVars)
+	if err != nil {
+		instance.Status.Conditions.Set(condition.FalseCondition(
+			condition.ServiceConfigReadyCondition,
+			condition.ErrorReason,
+			condition.SeverityWarning,
+			condition.ServiceConfigReadyErrorMessage,
+			err.Error()))
+		return ctrl.Result{}, err
+	} else if hashChanged {
+		// Hash changed and instance status should be updated (which will be done by main defer func),
+		// so we need to return and reconcile again
+		return ctrl.Result{}, nil
+	}
+
+	instance.Status.Hash[common.InputHashName] = inputHash
+
+	instance.Status.Conditions.MarkTrue(condition.ServiceConfigReadyCondition, condition.ServiceConfigReadyMessage)
+
+	_, _, err = logging.PatchNodeSet(instance, "logging", helper)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	_, _, err = logging.Deployment(instance, helper)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	serviceLabels := map[string]string{
+		common.AppSelector: logging.ServiceName,
+	}
+
+	_, _, err = logging.Service(instance, helper, serviceLabels)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	Log.Info("Reconciled Service successfully")
+	return ctrl.Result{}, nil
+}
+
+func (r *LoggingReconciler) generateComputeServiceConfig(
+	ctx context.Context,
+	h *helper.Helper,
+	instance *telemetryv1.Logging,
+	envVars *map[string]env.Setter,
+) error {
+
+	cmLabels := labels.GetLabels(instance, labels.GetGroupLabel(ceilometer.ComputeServiceName), map[string]string{})
+
+	templateParameters := map[string]interface{}{
+		"RsyslogAddress":         instance.Spec.IPAddr,
+		"RsyslogPort":            instance.Spec.Port,
+		"RsyslogProtocol":        instance.Spec.Protocol,
+		"RsyslogRetries":         instance.Spec.RsyslogRetries,
+		"RsyslogQueueType":       instance.Spec.RsyslogQueueType,
+		"RsyslogQueueSize":       instance.Spec.RsyslogQueueSize,
+		"RsyslogPersistInterval": instance.Spec.RsyslogPersistInterval,
+	}
+
+	cms := []util.Template{
+		// Secret
+		{
+			Name:          fmt.Sprintf("%s-config-data", logging.ComputeServiceName),
+			Namespace:     instance.Namespace,
+			Type:          util.TemplateTypeConfig,
+			InstanceType:  "logging",
+			ConfigOptions: templateParameters,
+			Labels:        cmLabels,
+		},
+	}
+	return secret.EnsureSecrets(ctx, h, instance, cms, envVars)
+}
+
+// createHashOfInputHashes - creates a hash of hashes which gets added to the resources which requires a restart
+// if any of the input resources change, like configs, passwords, ...
+//
+// returns the hash, whether the hash changed (as a bool) and any error
+func (r *LoggingReconciler) createHashOfInputHashes(
+	ctx context.Context,
+	instance *telemetryv1.Logging,
+	envVars map[string]env.Setter,
+) (string, bool, error) {
+	Log := r.GetLogger(ctx)
+	var hashMap map[string]string
+	changed := false
+	mergedMapVars := env.MergeEnvs([]corev1.EnvVar{}, envVars)
+	hash, err := util.ObjectHash(mergedMapVars)
+	if err != nil {
+		return hash, changed, err
+	}
+	if hashMap, changed = util.SetHash(instance.Status.Hash, common.InputHashName, hash); changed {
+		instance.Status.Hash = hashMap
+		Log.Info(fmt.Sprintf("Input maps hash %s - %s", common.InputHashName, hash))
+	}
+	return hash, changed, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *LoggingReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&telemetryv1.Logging{}).
+		Owns(&corev1.Secret{}).
+		Complete(r)
+}
---
+++ pkg/logging/dataplane.go
@@ -0,0 +1,73 @@
+package logging
+
+import (
+	"context"
+
+	dataplanev1 "github.com/openstack-k8s-operators/dataplane-operator/api/v1beta1"
+	helper "github.com/openstack-k8s-operators/lib-common/modules/common/helper"
+	telemetryv1 "github.com/openstack-k8s-operators/telemetry-operator/api/v1beta1"
+	"golang.org/x/exp/slices"
+	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+// PatchNodeSet patches the appropiate OpenStackDataPlaneNodeSet to add an OpenStackDataPlaneService
+func PatchNodeSet(instance *telemetryv1.Logging,
+	dataplaneServiceName string,
+	helper *helper.Helper,
+) (*dataplanev1.OpenStackDataPlaneNodeSet, controllerutil.OperationResult, error) {
+
+	logger := helper.GetLogger()
+
+	dataplaneNodeSet := &dataplanev1.OpenStackDataPlaneNodeSet{}
+	err := helper.GetClient().Get(context.TODO(), types.NamespacedName{
+		Name:      instance.Spec.NodeSetName,
+		Namespace: instance.Namespace,
+	}, dataplaneNodeSet)
+	if err != nil {
+		if k8s_errors.IsNotFound(err) {
+			logger.Error(err, "openstackdataplanenodeset not found")
+			return nil, controllerutil.OperationResultNone, err
+		}
+		logger.Error(err, "error retrieving openstackdataplanenodeset")
+		return nil, controllerutil.OperationResultNone, err
+	}
+	if !slices.Contains(dataplaneNodeSet.Spec.Services, dataplaneServiceName) {
+		op, err := controllerutil.CreateOrPatch(context.TODO(), helper.GetClient(), dataplaneNodeSet, func() error {
+			dataplaneNodeSet.Spec.Services = append(dataplaneNodeSet.Spec.Services, dataplaneServiceName)
+			return nil
+		})
+		if err != nil {
+			logger.Error(err, "error patching openstackdataplanenodeset")
+			return nil, controllerutil.OperationResultNone, err
+		}
+		return dataplaneNodeSet, op, nil
+	}
+
+	return nil, controllerutil.OperationResultNone, nil
+}
+
+// Deployment creates an OpenStackDataPlaneDeployment CR to trigger ansible execution
+func Deployment(instance *telemetryv1.Logging,
+	helper *helper.Helper) (*dataplanev1.OpenStackDataPlaneDeployment, controllerutil.OperationResult, error) {
+
+	logger := helper.GetLogger()
+
+	dataplaneDeployment := &dataplanev1.OpenStackDataPlaneDeployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Spec.NodeSetName + "-" + ServiceName,
+			Namespace: instance.Namespace,
+		},
+	}
+	op, err := controllerutil.CreateOrUpdate(context.TODO(), helper.GetClient(), dataplaneDeployment, func() error {
+		dataplaneDeployment.Spec.NodeSets = []string{instance.Spec.NodeSetName}
+		return nil
+	})
+	if err != nil {
+		logger.Error(err, "error creating openstackdataplanedeployment")
+		return nil, controllerutil.OperationResultNone, err
+	}
+	return dataplaneDeployment, op, nil
+}
