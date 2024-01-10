566cf1e25b7e6cd44d111a9d053d725184ec67a3
jlarriba <jlarriba@redhat.com>
2024-01-10 17:02:41 +0100

    Do not let the user choose syslog protocol

--- api/v1beta1/logging_types.go
+++ api/v1beta1/logging_types.go
@@ -44,12 +44,6 @@ type LoggingSpec struct {
 	// +kubebuilder:default=10514
 	TargetPort int `json:"targetPort"`
 
-	// The protocol of the connection the Service will listen on: tcp or udp
-	// +kubebuilder:validation:Optional
-	// +kubebuilder:validation:Enum=TCP;UDP
-	// +kubebuilder:default:=TCP
-	Protocol string `json:"protocol"`
-
 	// CLONamespace points to the namespace where the cluster-logging-operator is deployed
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:default=openshift-logging
