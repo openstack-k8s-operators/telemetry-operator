51fabcf675e5a7064f878076cd335a4a21c1c08f
Martin Mágr <mmagr@redhat.com>
2024-03-13 10:24:00 +0100

    Fix rewrite of Ceilometer ServiceMonitor

--- controllers/metricstorage_controller.go
+++ controllers/metricstorage_controller.go
@@ -186,6 +186,31 @@ func (r *MetricStorageReconciler) reconcileDelete(
 	return ctrl.Result{}, nil
 }
 
+// createServiceMonitor creates a ServiceMonitor CR for specified service
+func (r *MetricStorageReconciler) createServiceMonitor(
+	ctx context.Context,
+	instance *telemetryv1.MetricStorage,
+	serviceName string,
+	serviceLabels map[string]string,
+) (*monv1.ServiceMonitor, error) {
+	monitor := &monv1.ServiceMonitor{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-%s", instance.Name, serviceName),
+			Namespace: instance.Namespace,
+		},
+	}
+	_, err := controllerutil.CreateOrPatch(ctx, r.Client, monitor, func() error {
+		labels := map[string]string{
+			common.AppSelector: serviceName,
+		}
+		desiredMonitor := metricstorage.ServiceMonitor(instance, serviceLabels, labels)
+		desiredMonitor.Spec.DeepCopyInto(&monitor.Spec)
+		monitor.ObjectMeta.Labels = desiredMonitor.ObjectMeta.Labels
+		return controllerutil.SetControllerReference(instance, monitor, r.Scheme)
+	})
+	return monitor, err
+}
+
 func (r *MetricStorageReconciler) reconcileNormal(
 	ctx context.Context,
 	instance *telemetryv1.MetricStorage,
@@ -274,7 +299,6 @@ func (r *MetricStorageReconciler) reconcileNormal(
 
 	// Deploy ServiceMonitor for ceilometer and kube-state-metrics monitoring
 	err = r.ensureWatches(ctx, "servicemonitors.monitoring.rhobs", &monv1.ServiceMonitor{}, eventHandler)
-
 	if err != nil {
 		instance.Status.Conditions.MarkFalse(telemetryv1.ServiceMonitorReadyCondition,
 			condition.Reason("Can't own ServiceMonitor resource"),
@@ -283,45 +307,15 @@ func (r *MetricStorageReconciler) reconcileNormal(
 		Log.Info("Can't own ServiceMonitor resource")
 		return ctrl.Result{RequeueAfter: telemetryv1.PauseBetweenWatchAttempts}, nil
 	}
-	ceilometerMonitor := &monv1.ServiceMonitor{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name,
-			Namespace: instance.Namespace,
-		},
-	}
-	op, err = controllerutil.CreateOrPatch(ctx, r.Client, ceilometerMonitor, func() error {
-		ceilometerLabels := map[string]string{
-			common.AppSelector: ceilometer.ServiceName,
-		}
-		desiredCeilometerMonitor := metricstorage.ServiceMonitor(instance, serviceLabels, ceilometerLabels)
-		desiredCeilometerMonitor.Spec.DeepCopyInto(&ceilometerMonitor.Spec)
-		ceilometerMonitor.ObjectMeta.Labels = desiredCeilometerMonitor.ObjectMeta.Labels
-		err = controllerutil.SetControllerReference(instance, ceilometerMonitor, r.Scheme)
-		return err
-	})
+
+	ceilometerMonitor, err := r.createServiceMonitor(ctx, instance, ceilometer.ServiceName, serviceLabels)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
 	if op != controllerutil.OperationResultNone {
 		Log.Info(fmt.Sprintf("Ceilometer ServiceMonitor %s successfully changed - operation: %s", ceilometerMonitor.Name, string(op)))
 	}
-
-	ksmMonitor := &monv1.ServiceMonitor{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name,
-			Namespace: instance.Namespace,
-		},
-	}
-	op, err = controllerutil.CreateOrPatch(ctx, r.Client, ksmMonitor, func() error {
-		ksmLabels := map[string]string{
-			common.AppSelector: availability.KSMServiceName,
-		}
-		desiredKSMMonitor := metricstorage.ServiceMonitor(instance, serviceLabels, ksmLabels)
-		desiredKSMMonitor.Spec.DeepCopyInto(&ksmMonitor.Spec)
-		ksmMonitor.ObjectMeta.Labels = desiredKSMMonitor.ObjectMeta.Labels
-		err = controllerutil.SetControllerReference(instance, ksmMonitor, r.Scheme)
-		return err
-	})
+	ksmMonitor, err := r.createServiceMonitor(ctx, instance, availability.KSMServiceName, serviceLabels)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
@@ -329,6 +323,7 @@ func (r *MetricStorageReconciler) reconcileNormal(
 		Log.Info(fmt.Sprintf("KSM ServiceMonitor %s successfully changed - operation: %s", ksmMonitor.Name, string(op)))
 	}
 	instance.Status.Conditions.MarkTrue(telemetryv1.ServiceMonitorReadyCondition, condition.ReadyMessage)
+
 	// Deploy ScrapeConfig for NodeExporter monitoring
 	nodeSetWatchFn := func(ctx context.Context, o client.Object) []reconcile.Request {
 		// Reconcile all metricstorages when a nodeset changes
