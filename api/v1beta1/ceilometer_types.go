a42b3ffa1ebb8a51bc4fe7c835df4088dccbaf56
Jaromir Wysoglad <jwysogla@redhat.com>
2024-04-17 03:28:16 -0400

    [OSPRH-6400] Rename ceilometer proxy image
    This is a part of a change to include the ceilometer proxy
    image into openstack-operator version. The rename makes it
    more consistent with other openstack images, which also
    prepend the service name befor the image.
    
    rh-jira: https://issues.redhat.com/browse/OSPRH-6400

--- api/v1beta1/ceilometer_types.go
+++ api/v1beta1/ceilometer_types.go
@@ -35,8 +35,8 @@ const (
 	CeilometerComputeContainerImage = "quay.io/podified-antelope-centos9/openstack-ceilometer-compute:current-podified"
 	// CeilometerIpmiContainerImage - default fall-back image for Ceilometer Ipmi
 	CeilometerIpmiContainerImage = "quay.io/podified-antelope-centos9/openstack-ceilometer-ipmi:current-podified"
-	// ProxyContainerImage - default fall-back image for proxy container
-	ProxyContainerImage     = "quay.io/podified-antelope-centos9/openstack-aodh-api:current-podified"
+	// CeilometerProxyContainerImage - default fall-back image for proxy container
+	CeilometerProxyContainerImage     = "quay.io/podified-antelope-centos9/openstack-aodh-api:current-podified"
 )
 
 // CeilometerSpec defines the desired state of Ceilometer
@@ -175,7 +175,7 @@ func SetupDefaultsCeilometer() {
 		NotificationContainerImageURL: util.GetEnvVar("RELATED_IMAGE_CEILOMETER_NOTIFICATION_IMAGE_URL_DEFAULT", CeilometerNotificationContainerImage),
 		ComputeContainerImageURL:      util.GetEnvVar("RELATED_IMAGE_CEILOMETER_COMPUTE_IMAGE_URL_DEFAULT", CeilometerComputeContainerImage),
 		IpmiContainerImageURL:         util.GetEnvVar("RELATED_IMAGE_CEILOMETER_IPMI_IMAGE_URL_DEFAULT", CeilometerIpmiContainerImage),
-		ProxyContainerImageURL:         util.GetEnvVar("RELATED_IMAGE_CEILOMETER_PROXY_IMAGE_URL_DEFAULT", ProxyContainerImage),
+		ProxyContainerImageURL:        util.GetEnvVar("RELATED_IMAGE_CEILOMETER_PROXY_IMAGE_URL_DEFAULT", CeilometerProxyContainerImage),
 	}
 
 	SetupCeilometerDefaults(ceilometerDefaults)
