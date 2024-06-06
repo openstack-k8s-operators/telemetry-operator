22842c071a5ae4a7931846916c036258ec4aea13
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2024-06-06 14:56:25 +0100

    Get data from dataplane inventory secret
    https://github.com/openstack-k8s-operators/dataplane-operator/pull/911
    
    Signed-off-by: Fabricio Aguiar <fabricio.aguiar@gmail.com>

--- main.go
+++ main.go
@@ -39,7 +39,6 @@ import (
 
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
-	dataplanev1 "github.com/openstack-k8s-operators/dataplane-operator/api/v1beta1"
 	heatv1 "github.com/openstack-k8s-operators/heat-operator/api/v1beta1"
 	infranetworkv1 "github.com/openstack-k8s-operators/infra-operator/apis/network/v1beta1"
 	rabbitmqv1 "github.com/openstack-k8s-operators/infra-operator/apis/rabbitmq/v1beta1"
@@ -70,7 +69,6 @@ func init() {
 	utilruntime.Must(monv1alpha1.AddToScheme(scheme))
 	utilruntime.Must(mariadbv1beta1.AddToScheme(scheme))
 	utilruntime.Must(heatv1.AddToScheme(scheme))
-	utilruntime.Must(dataplanev1.AddToScheme(scheme))
 	utilruntime.Must(infranetworkv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
