689dddb9dfdf57fe30ffef596c90a51cec76de2e
Martin Schuppert <mschuppert@redhat.com>
2024-10-29 18:00:03 +0100

    Switch to use new CreateServiceReadyCondition
    The current ExposeServiceReadyCondition can be confusing since
    it just reflects if the k8s service got created, but not the route
    to expose the public endpoint. This changes to a new
    CreateServiceReadyCondition to prevent this.
    
    Jira: OSPRH-4164
    
    Depends-On: https://github.com/openstack-k8s-operators/lib-common/pull/572
    
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- controllers/aodh_controller.go
+++ controllers/aodh_controller.go
@@ -403,10 +403,10 @@ func (r *AutoscalingReconciler) reconcileNormalAodh(
 		)
 		if err != nil {
 			instance.Status.Conditions.Set(condition.FalseCondition(
-				condition.ExposeServiceReadyCondition,
+				condition.CreateServiceReadyCondition,
 				condition.ErrorReason,
 				condition.SeverityWarning,
-				condition.ExposeServiceReadyErrorMessage,
+				condition.CreateServiceReadyErrorMessage,
 				err.Error()))
 
 			return ctrl.Result{}, err
@@ -435,19 +435,19 @@ func (r *AutoscalingReconciler) reconcileNormalAodh(
 		ctrlResult, err := svc.CreateOrPatch(ctx, helper)
 		if err != nil {
 			instance.Status.Conditions.Set(condition.FalseCondition(
-				condition.ExposeServiceReadyCondition,
+				condition.CreateServiceReadyCondition,
 				condition.ErrorReason,
 				condition.SeverityWarning,
-				condition.ExposeServiceReadyErrorMessage,
+				condition.CreateServiceReadyErrorMessage,
 				err.Error()))
 
 			return ctrlResult, err
 		} else if (ctrlResult != ctrl.Result{}) {
 			instance.Status.Conditions.Set(condition.FalseCondition(
-				condition.ExposeServiceReadyCondition,
+				condition.CreateServiceReadyCondition,
 				condition.RequestedReason,
 				condition.SeverityInfo,
-				condition.ExposeServiceReadyRunningMessage))
+				condition.CreateServiceReadyRunningMessage))
 			return ctrlResult, nil
 		}
 		// create service - end
@@ -464,7 +464,7 @@ func (r *AutoscalingReconciler) reconcileNormalAodh(
 			return ctrl.Result{}, err
 		}
 	}
-	instance.Status.Conditions.MarkTrue(condition.ExposeServiceReadyCondition, condition.ExposeServiceReadyMessage)
+	instance.Status.Conditions.MarkTrue(condition.CreateServiceReadyCondition, condition.CreateServiceReadyMessage)
 
 	if instance.Status.APIEndpoints == nil {
 		instance.Status.APIEndpoints = map[string]string{}
