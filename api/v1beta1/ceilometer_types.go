748072dcc609b0f981616669647299dca9216204
Francesco Pantano <fpantano@redhat.com>
2025-02-13 09:32:39 +0100

    Consume Topology CR by reference
    This patch provides more granular control over Pod placement and
    scheduling through the Topology CR integration introduced in
    infra-operator.
    In particular it provides:
    
    - a new API parameter (TopologyRef) defined for each Component that
      allows to reference an existing Topology CRs in the same namespace
    
    - the operator logic that retrieves and processes the referenced
      Topology CR through the functions provided by infra-operator
    
    - enhanced StatefulSet and deployment configuration that incorporates
      the processed Topology
    
    - a set of kuttl tests to double check the lifecycle
      (add/update/override/remove) of the resulting Deployments and
      DaemonSet when a Topology is referenced
    
    Note that webhooks are in place to prevent referencing a Topology from a
    different namespace (which is not a supported scenario).
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- api/v1beta1/ceilometer_types.go
+++ api/v1beta1/ceilometer_types.go
@@ -19,6 +19,7 @@ package v1beta1
 import (
 	condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	topologyv1 "github.com/openstack-k8s-operators/infra-operator/apis/topology/v1beta1"
 
 	"github.com/openstack-k8s-operators/lib-common/modules/common/tls"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/util"
@@ -149,6 +150,11 @@ type CeilometerSpecCore struct {
 	// +kubebuilder:validation:Optional
 	// NodeSelector to target subset of worker nodes running this service
 	NodeSelector *map[string]string `json:"nodeSelector,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// TopologyRef to apply the Topology defined by the associated CR referenced
+	// by name
+	TopologyRef *topologyv1.TopoRef `json:"topologyRef,omitempty"`
 }
 
 // CeilometerStatus defines the observed state of Ceilometer
@@ -189,6 +195,9 @@ type CeilometerStatus struct {
 
 	// Map of hashes to track e.g. job status
 	KSMHash map[string]string `json:"ksmHash,omitempty"`
+
+	// LastAppliedTopology - the last applied Topology
+	LastAppliedTopology string `json:"lastAppliedTopology,omitempty"`
 }
 
 // NOTE(mmagr): remove KSMStatus with API version increment
