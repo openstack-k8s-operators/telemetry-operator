a62d38977d55ec1c0ef7b95424b283a52b53fc81
Martin Schuppert <mschuppert@redhat.com>
2025-04-16 16:45:41 +0200

    fix golangci reported issues
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- controllers/aodh_controller.go
+++ controllers/aodh_controller.go
@@ -537,7 +537,7 @@ func (r *AutoscalingReconciler) reconcileNormalAodh(
 					condition.TLSInputReadyCondition,
 					condition.RequestedReason,
 					condition.SeverityInfo,
-					fmt.Sprintf(condition.TLSInputReadyWaitingMessage, instance.Spec.Aodh.TLS.CaBundleSecretName)))
+					condition.TLSInputReadyWaitingMessage, instance.Spec.Aodh.TLS.CaBundleSecretName))
 				return ctrl.Result{}, nil
 			}
 			instance.Status.Conditions.Set(condition.FalseCondition(
@@ -561,7 +561,7 @@ func (r *AutoscalingReconciler) reconcileNormalAodh(
 					condition.TLSInputReadyCondition,
 					condition.RequestedReason,
 					condition.SeverityInfo,
-					fmt.Sprintf(condition.TLSInputReadyWaitingMessage, err.Error())))
+					condition.TLSInputReadyWaitingMessage, err.Error()))
 				return ctrl.Result{}, nil
 			}
 			instance.Status.Conditions.Set(condition.FalseCondition(
