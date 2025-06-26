94970893cfcf9fdfa659d2bd23924ce1ca0cc56e
mgirgisf <mgirgisf@redhat.com>
2025-06-26 09:04:28 +0200

    Update the secretName

--- pkg/metricstorage/prometheus_proxy_patch.go
+++ pkg/metricstorage/prometheus_proxy_patch.go
@@ -45,7 +45,7 @@ func PrometheusProxy(instance *telemetryv1.MetricStorage) *unstructured.Unstruct
 					map[string]interface{}{
 						"name": "ca-cert",
 						"secret": map[string]interface{}{
-							"secretName": "prometheus-metric-storage",
+							"secretName": "cert-metric-storage-sample-prometheus-svc",
 							"items": []interface{}{
 								map[string]interface{}{
 									"key":  "ca.crt",
