152e02b8cee39ce08475c593dcac21d40e9bdcc3
Emma Foley <efoley@redhat.com>
2025-09-17 11:32:31 -0400

    fix undefined: intstr

--- pkg/cloudkittyproc/statefulset.go
+++ pkg/cloudkittyproc/statefulset.go
@@ -24,7 +24,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	// "k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 const (
